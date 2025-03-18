fdc7a8518c4656be2cf7ca0596c5da3b07639061
ShuKun.Li <2562017477@qq.com>
2025-03-18 22:29:57 +0800

    test: op

--- xuecheng-plus-system/xuecheng-plus-system-service/src/main/java/com/xuecheng/system/mapper/DictionaryMapper.java
+++ xuecheng-plus-system/xuecheng-plus-system-service/src/main/java/com/xuecheng/system/mapper/DictionaryMapper.java
@@ -4,9 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.system.model.po.Dictionary;
 
 /**
- * <p>
+ *
  * 数据字典 Mapper 接口
- * </p>
+ *
  *
  * @author itcast
  */
