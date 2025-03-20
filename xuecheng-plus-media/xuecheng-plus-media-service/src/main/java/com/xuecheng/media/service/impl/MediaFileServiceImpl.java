6dc5fcc9af2f72f74ee00f21a82a1db2a5038224
ShuKun.Li <2562017477@qq.com>
2025-03-20 16:25:02 +0800

    feat: xxljob 添加待处理任务

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
@@ -9,10 +9,12 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.base.model.RestResponse;
 import com.xuecheng.media.mapper.MediaFilesMapper;
+import com.xuecheng.media.mapper.MediaProcessMapper;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
 import com.xuecheng.media.model.dto.UploadFileParamsDto;
 import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
+import com.xuecheng.media.model.po.MediaProcess;
 import com.xuecheng.media.service.MediaFileService;
 import io.minio.*;
 
@@ -52,6 +54,9 @@ public class MediaFileServiceImpl implements MediaFileService {
 
     @Autowired
     MediaFilesMapper mediaFilesMapper;
+
+    @Autowired
+    MediaProcessMapper mediaProcessMapper;
 
     @Autowired
     MinioClient minioClient;
@@ -183,8 +188,35 @@ public class MediaFileServiceImpl implements MediaFileService {
             }
             log.debug("保存文件信息到数据库成功,{}", mediaFiles.toString());
 
+            addWaitingTask(mediaFiles);
+            log.debug("保存文件信息到数据库成功,{}", mediaFiles.toString());
         }
         return mediaFiles;
+
+    }
+
+
+    /**
+     * 添加待处理任务
+     *
+     * @param mediaFiles
+     */
+    private void addWaitingTask(MediaFiles mediaFiles) {
+        // 文件名称
+        String filename = mediaFiles.getFilename();
+        // 文件拓展名
+        String extension = filename.substring(filename.lastIndexOf("."));
+        // 文件mimeType
+        String mimeType = getMimeType(extension);
+        // 如果是avi视频添加到视频待处理表
+        if (mimeType.equals("video/x-msvideo")) {
+            MediaProcess mediaProcess = new MediaProcess();
+            BeanUtils.copyProperties(mediaFiles, mediaProcess);
+            mediaProcess.setStatus("1");// 未处理
+            mediaProcess.setFailCount(0);// 失败次数默认为0
+            mediaProcessMapper.insert(mediaProcess);
+        }
+
 
     }
 
