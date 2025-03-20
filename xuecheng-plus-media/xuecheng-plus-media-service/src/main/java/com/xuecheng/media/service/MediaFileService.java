f502cabc3fe92a54691dd44336099a0a8843f0e6
ShuKun.Li <2562017477@qq.com>
2025-03-20 11:04:22 +0800

    feat: 断点续传

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -3,28 +3,62 @@ package com.xuecheng.media.service;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 /**
- * @description 媒资文件管理业务类
  * @author Mr.M
- * @date 2022/9/10 8:55
  * @version 1.0
+ * @description 媒资文件管理业务类
+ * @date 2022/9/10 8:55
  */
 public interface MediaFileService {
 
- /**
-  * @description 媒资文件查询方法
-  * @param pageParams 分页参数
-  * @param queryMediaParamsDto 查询条件
-  * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
-  * @author Mr.M
-  * @date 2022/9/10 8:57
- */
- public PageResult<MediaFiles> queryMediaFiels(Long companyId,PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+    /**
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
+     * @description 媒资文件查询方法
+     * @author Mr.M
+     * @date 2022/9/10 8:57
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
 
+    /*
+     *//**
+     * @description 上传文件的通用接口
+     * @param companyId           机构id
+     * @param uploadFileParamsDto 文件信息
+     * @param bytes               文件字节数组
+     * @param folder              桶下边的子目录
+     * @param objectName          对象名称
+     * @return com.xuecheng.media.model.dto.UploadFileResultDto
+     *//*
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, byte[] bytes, String folder, String objectName);*/
+
+    /**
+     * 上传文件
+     *
+     * @param companyId           机构id
+     * @param uploadFileParamsDto 上传文件信息
+     * @param localFilePath       文件磁盘路径
+     * @return 文件信息
+     */
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
+
+    /**
+     * 文件对象连接存入数据库
+     * @param companyId
+     * @param fileMd5
+     * @param uploadFileParamsDto
+     * @param bucket
+     * @param objectName
+     * @return
+     */
+    MediaFiles addMediaFilesToDb(Long companyId, String fileMd5, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
 
 }
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/minio/MinioTest.java
@@ -0,0 +1,75 @@
+package minio;
+
+import io.minio.*;
+import io.minio.errors.MinioException;
+import org.apache.commons.compress.utils.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @description 测试MinIO
+ * @author Mr.M
+ * @date 2022/9/11 21:24
+ * @version 1.0
+ */
+public class MinioTest {
+
+    static MinioClient minioClient =
+            MinioClient.builder()
+                    .endpoint("http://192.168.101.65:9000")
+                    .credentials("minioadmin", "minioadmin")
+                    .build();
+
+   //上传文件
+    @Test
+    public  void upload() {
+        try {
+            UploadObjectArgs testbucket = UploadObjectArgs.builder()
+                    .bucket("testbucket")
+                    //.object("test001.mp4")
+                    .object("001/test001.mp4")//添加子目录
+                    .filename("D:\\develop\\upload\\1mp4.temp")
+                    .contentType("video/mp4")//默认根据扩展名确定文件内容类型，也可以指定
+                    .build();
+            minioClient.uploadObject(testbucket);
+            System.out.println("上传成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("上传失败");
+        }
+
+    }
+
+    @Test
+    public void delete(){
+        try {
+            minioClient.removeObject(
+                    RemoveObjectArgs.builder().bucket("testbucket").object("001/test001.mp4").build());
+            System.out.println("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("删除失败");
+        }
+    }
+
+    //查询文件
+    @Test
+    public void getFile() {
+        GetObjectArgs getObjectArgs = GetObjectArgs.builder().bucket("testbucket").object("test001.mp4").build();
+        try(
+                FilterInputStream inputStream = minioClient.getObject(getObjectArgs);
+                FileOutputStream outputStream = new FileOutputStream(new File("D:\\develop\\upload\\1_2.mp4"));
+        ) {
+            IOUtils.copy(inputStream,outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
